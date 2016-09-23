dc03ad602c493a9d4253cb8e05788ec2c46df098
Michael Lin <linzichunzf@hotmail.com>
2016-09-22 23:19:45 -0600

    add HabitHistoryActivity

---
+++ app/src/main/java/org/vfree/zichun3_habittrack/HabitHistoryActivity.java
@@ -0,0 +1,13 @@
+package org.vfree.zichun3_habittrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HabitHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_history);
+    }
+}
